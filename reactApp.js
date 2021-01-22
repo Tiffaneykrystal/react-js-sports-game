5d73cc183cbd988365187d49503de5cd5a61471e
Tiffaney bolich <tiffaneykrystal@gmail.com>
2021-01-22 17:13:43 -0500

    Initial Commit

---
+++ reactApp.js
@@ -0,0 +1,102 @@
+class Team extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            shots: 0,
+            score: 0
+        }
+    
+    this.shotSound = new Audio("./assets/audio/Swish.wav")
+    this.scoreSound = new Audio("./assets/audio/cheers.wav")
+    }
+
+
+
+    shotHandler = () => {
+        let score = this.state.score
+        this.shotSound.play()
+        
+        if (Math.random() > 0.5) {
+            score += 1
+
+            setTimeout(() => {
+                this.scoreSound.play()
+              }, 100)
+        }
+
+        this.setState((state, props) => ({
+            shots: state.shots + 1,
+            score
+
+        }))
+    }
+    render() {
+        let shotPercentageDiv
+
+        if (this.state.shots){
+        const shotPercentage = Math.round((this.state.score / this.state.shots) * 100)
+        shotPercentageDiv = (
+            <div>
+                <strong> Shooting %: {shotPercentage} </strong>
+            </div>
+        )
+        }
+        return (
+            <div className="Team">
+                <h2>{this.props.name}</h2>
+
+                <div className="identity">
+                    <img src={this.props.logo} alt={this.props.name} />
+                </div>
+
+                <div>
+                    <strong>Shots:</strong> {this.state.shots}
+                </div>
+
+                <div>
+                    <strong>Score:</strong> {this.state.score}
+                </div>
+
+               {shotPercentageDiv} 
+
+                <button onClick={this.shotHandler}>Shoot!</button>
+            </div>
+        )
+    }
+}
+
+function Game (props) {
+  return (
+      <div className="Game">
+        <h1> Welcome to {props.venue}</h1>
+          <div className="stats">
+            <Team name="Detroit Punishers" 
+            logo="assets\images\detroit.jpg"
+             />
+
+            <div className="versus">
+                 <h1>VS</h1>
+            </div>
+
+            <Team name="Battle Creek Beavers" 
+             logo="assets\images\beaver.jpg"
+             />
+        </div> 
+      </div>
+  )  
+}
+
+function App(props){
+    return (
+      <div className="App">
+       <Game venue= "Cobo Hall" />
+       <Game venue= "Beaver Arena"/>
+      </div>
+    )
+  }
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
